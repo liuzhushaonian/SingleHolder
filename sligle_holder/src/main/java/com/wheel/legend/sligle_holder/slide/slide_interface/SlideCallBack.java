6e07aeb3874d64a30f75be53381d4cdfb7c102a0
legend <liuzhushaonian@sina.cn>
2019-05-30 14:58:07 +0800

    complete 1.0

---
+++ sligle_holder/src/main/java/com/wheel/legend/sligle_holder/slide/slide_interface/SlideCallBack.java
@@ -0,0 +1,13 @@
+package com.wheel.legend.sligle_holder.slide.slide_interface;
+
+public interface SlideCallBack {
+
+
+    void slideComplete();
+
+    void slideClose();
+
+    void setActivity();
+
+
+}
